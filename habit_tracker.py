ca1423f065c18d63dc8f3049c0775fab9ab187f2
Shakti Roul <shakti.roul@gmail.com>
2026-01-18 08:02:38 +0530

    habit_tracker

---
+++ habit_tracker.py
@@ -0,0 +1,41 @@
+import requests
+
+USERNAME = "shakti9"
+TOKEN = "bcsjdcghcgbui5876"
+GRAPH_ID = "graph1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes", 
+}
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+pixel_data = {
+    "date": "20250117"
+    "quantity": 
+}
